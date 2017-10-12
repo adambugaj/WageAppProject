b6077aa56e77fbbba9f09c3982e034658a5acd36
Adam Bugaj <adampeter.bugaj@gmail.com>
2017-10-12 16:25:19 +0200

    Hide and show divs added, project released

--- wageUp.js
+++ wageUp.js
@@ -1,42 +1,78 @@
+function hideDiv() {
+    var getDiv = document.getElementById("resultMonthly");
+    getDiv.style.display = "none";
+     var getDiv = document.getElementById("resultDaily");
+    getDiv.style.display = "none";
+     var getDiv = document.getElementById("resultHourly");
+    getDiv.style.display = "none";
+     var getDiv = document.getElementById("devOutput");
+    getDiv.style.display = "none";     
+}
+
 function wage() {
     let input = document.getElementById('annualInput').value;
-    let output = document.getElementById('resultMonthly');
+    var output = document.getElementById('resultMonthly');
     // it doesn't work, but it works with pure document.getElementById('result').innerHTML = input;
-    output.innerHTML = input;
+   
     
     
     // annual wage devided by 12 months
-    let resultMonthly = input / 12;
-    let showMonthly = 'Your monthly wage: ' + resultMonthly.toFixed(2);
+    var resultMonthly = input / 12;
+    var showMonthly = 'Your monthly wage: ' + resultMonthly.toFixed(2);
     
     // monthly wage devided by 20 days
-    let resultDaily = resultMonthly / 20;
-    let showDaily = 'Your daily wage: ' + resultDaily.toFixed(2);
+    var resultDaily = resultMonthly / 20;
+    var showDaily = 'Your daily wage: ' + resultDaily.toFixed(2);
     
     // daily wage devided by 8 hours
-    let resultHourly = resultDaily / 8;
-    let showHourly = 'Your hourly wage: ' + resultHourly.toFixed(2);
+    var resultHourly = resultDaily / 8;
+    var showHourly = 'Your hourly wage: ' + resultHourly.toFixed(2);
+   
+    var getValue = document.getElementById("annualInput").value
     
-    input = +input;
-
-     //check if input is empty and if string is number
-     if (input) {
-         if (isNaN(input) !== true) {
+    var getNumber = Number(getValue);
+    var numberValue = +getValue || getValue;
+    
+    //check whether is string or number
+    alert(typeof numberValue);
+    
+    
+     //check if input is empty and if string is a number
+     if (getValue !== '') {
+         if (!isNaN(getNumber)) {
+             
             document.getElementById('resultMonthly').innerHTML = showMonthly + ' $';
             document.getElementById('resultDaily').innerHTML = showDaily + ' $';
             document.getElementById('resultHourly').innerHTML = showHourly + ' $';
-         } 
-         else {
-        document.getElementById('resultMonthly').innerHTML = 'Please, type your annual wage';
+         } else {
+              alert("This is not a number, please try again");
+         }
+     } else {
+        document.getElementById('resultMonthly').innerHTML = "Please, type your annual wage";
         document.getElementById('resultDaily').innerHTML = "e.g 125000";
         document.getElementById('resultHourly').innerHTML = "and click the button 'check'";
          }
-     }
+    
+    
+    var getDiv = document.getElementById("resultMonthly");
+    var getDiv1 = document.getElementById("resultDaily");
+    var getDiv2 = document.getElementById("resultHourly");
+    var getDiv3 = document.getElementById("devOutput");
+ 
+    
+    if (getDiv) {
+                 getDiv.style.display = "block";
+                 getDiv1.style.display = "block";
+                 getDiv2.style.display = "block";
+                 getDiv3.style.display = "block";
+             } else {
+                 getDiv.style.display = "none";
+             }
 
     
     // switch option for wage occupation
-    let storeWage;
-    let dev = document.getElementById('dev').value;
+    var storeWage;
+    var dev = document.getElementById('dev').value;
     
     switch(dev) {
         case "junior developer":
@@ -54,31 +90,4 @@ function wage() {
     document.getElementById('devOutput').innerHTML = "Estimated wage for " + dev + ": " + storeWage;
     
     
-      //  check and test
-    let getValue = document.getElementById("annualInput").value
-    
-//    let numberValue = +getValue || getValue;
-//    alert(typeof numberValue);
-//   
-//    if (numberValue !== '') {
-//        // this if doesn't work even with string and number
-//         if (typeof numberValue == 'number')
-//            document.getElementById('devOutput').innerHTML = "great " + numberValue;
-//        else alert("try again")
-//    } else {
-//        document.getElementById('devOutput').innerHTML = 'Please, type your annual wage';
-//    }
-    if (!isNaN(getValue)) {
-        document.getElementById("devOutput").innerHTML = getValue;Number(getValue);
-    } else {
-        document.getElementById('devOutput').innerHTML = 'Please, type your annual wage';
-    }
-    alert(typeof getValue)
-//    let numberValue = !isNaN(Number(getValue)) ? Number(getValue) : document.getElementById('devOutput').innerHTML = 'Please, type your annual wage';
-//    alert(typeof numberValue);
-    
 }
-
-
-
-
